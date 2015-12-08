a01e0fd57a798013fd13498126da654d0e28cb6d
Hussein <debelhussein@gmail.com>
2015-12-08 23:36:18 +1100

    day3 part2 completed

--- day3/day3p2.py
+++ day3/day3p2.py
@@ -2,13 +2,24 @@
 
 from collections import defaultdict
 map = defaultdict(int)
-
 unique_houses = 1
 filename = "day3_input"
 
-x_coord = 0
-y_coord = 0
-map[x_coord, y_coord] += 1
+x_coord_santa = 0
+y_coord_santa = 0
+
+x_coord_robosanta = 0
+y_coord_robosanta = 0
+
+map[x_coord_santa, y_coord_santa] += 1
+
+num = 0
+
+def new_location(map, x, y, houses):
+	map[x, y] += 1
+	if map[x, y] == 1:
+		houses += 1
+	return (map, x, y, houses)
 
 with open(filename) as f:
 	while True:
@@ -16,24 +27,33 @@ with open(filename) as f:
 		if not c:
 			break
 		if c == "^":
-			y_coord += 1
-			map[x_coord, y_coord] += 1
-			if map[x_coord, y_coord] == 1:
-				unique_houses += 1			
+			if num % 2 == 0:
+				y_coord_santa += 1
+				(map, x_coord_santa, y_coord_santa, unique_houses) = new_location(map, x_coord_santa, y_coord_santa, unique_houses)
+			else:
+				y_coord_robosanta += 1
+				(map, x_coord_robosanta, y_coord_robosanta, unique_houses) = new_location(map, x_coord_robosanta, y_coord_robosanta, unique_houses)
 		elif c == "v":
-			y_coord -= 1
-			map[x_coord, y_coord] += 1
-			if map[x_coord, y_coord] == 1:
-				unique_houses += 1
+			if num % 2 == 0:
+				y_coord_santa -= 1
+				(map, x_coord_santa, y_coord_santa, unique_houses) = new_location(map, x_coord_santa, y_coord_santa, unique_houses)
+			else:
+				y_coord_robosanta -= 1
+				(map, x_coord_robosanta, y_coord_robosanta, unique_houses) = new_location(map, x_coord_robosanta, y_coord_robosanta, unique_houses)
 		elif c == ">":
-			x_coord += 1
-			map[x_coord, y_coord] += 1
-			if map[x_coord, y_coord] == 1:
-				unique_houses += 1
+			if num % 2 == 0:
+				x_coord_santa += 1
+				(map, x_coord_santa, y_coord_santa, unique_houses) = new_location(map, x_coord_santa, y_coord_santa, unique_houses)
+			else:
+				x_coord_robosanta += 1
+				(map, x_coord_robosanta, y_coord_robosanta, unique_houses) = new_location(map, x_coord_robosanta, y_coord_robosanta, unique_houses)
 		elif c == "<":
-			x_coord -= 1
-			map[x_coord, y_coord] += 1
-			if map[x_coord, y_coord] == 1:
-				unique_houses += 1
-		
+			if num % 2 == 0:
+				x_coord_santa -= 1
+				(map, x_coord_santa, y_coord_santa, unique_houses) = new_location(map, x_coord_santa, y_coord_santa, unique_houses)
+			else:
+				x_coord_robosanta -= 1
+				(map, x_coord_robosanta, y_coord_robosanta, unique_houses) = new_location(map, x_coord_robosanta, y_coord_robosanta, unique_houses)
+		num += 1
+
 print unique_houses
