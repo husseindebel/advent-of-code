afc5f60b67c222ab660528bfdcdc2bdfd413bfe0
Hussein <debelhussein@gmail.com>
2015-12-09 23:14:37 +1100

    Part2 added to a different fike

--- day4/day4p2.py
+++ day4/day4p2.py
@@ -2,14 +2,14 @@ import hashlib
 import re
 
 key = "iwrupvqb"
-num = 0
+num = 346386 ## from the previous answer
 
 while True:
     encoded = hashlib.md5()
     new_key = key+str(num)
     encoded.update(new_key)
     hash = encoded.hexdigest()
-    if re.match(r"^00000", hash):
+    if re.match(r"^000000", hash):
         print "Done"
         break
     num += 1
