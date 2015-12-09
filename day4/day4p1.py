84cd35873031c430f102221111adb8aa4d690d7e
Hussein <debelhussein@gmail.com>
2015-12-09 23:12:48 +1100

    Day 4 Completed

---
+++ day4/day4p1.py
@@ -0,0 +1,17 @@
+import hashlib
+import re
+
+key = "iwrupvqb"
+num = 346386
+
+while True:
+    encoded = hashlib.md5()
+    new_key = key+str(num)
+    encoded.update(new_key)
+    hash = encoded.hexdigest()
+    if re.match(r"^000000", hash):
+        print "Done"
+        break
+    num += 1
+
+print num
